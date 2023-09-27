182ec954379302bbe1c3e281b88e8703c3bbe031
dany <danyirfansyah23@gmail.com>
2023-09-27 09:25:55 +0700

    Jurnal-01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "Dany Irfansyah";
+        String nim = "607062300067";
+        String kelas = "D3IF-47-04";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
